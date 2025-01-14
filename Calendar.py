e08d2ece1a5a3e9daa94f177934fbc2aafade1fc
Viktoriia Kazniienko <nk.akk16@gmail.com>
2025-01-14 20:57:38 +0200

    add calendar that shows today's date and the current month by library Tkinter

--- Calendar.py
+++ Calendar.py
@@ -1,7 +1,23 @@
-from tkinter import *
+from tkinter import Tk, Label
+from datetime import datetime
+import calendar
+
+def get_calendar():
+    now = datetime.now()
+    year, month, day = now.year, now.month, now.day
+    cal_text = calendar.month(year, month)
+    today_data = f"Today: {day:02}.{month:02}.{year}"
+    return cal_text, today_data
 
 root = Tk()
-
 root.title("CALENDAR")
-root.maxsize(1920,1080)
+root.configure(bg="#6495ED")
+calendar_text, today_data = get_calendar()
+
+cal_label = Label(root, text=calendar_text, font=("Courier", 12), justify="left")
+cal_label.pack()
+
+date_label = Label(root, text=today_data, font=("Arial", 14), fg="white", bg="#6495ED")
+date_label.pack()
+
 root.mainloop()
