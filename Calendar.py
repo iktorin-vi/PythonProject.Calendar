e66461d8da890f0fc97ea884c39c3d81d741a8c8
Viktoriia Kazniienko <nk.akk16@gmail.com>
2025-01-16 23:13:02 +0200

    add new function, new comments, minsize of window

--- Calendar.py
+++ Calendar.py
@@ -1,8 +1,15 @@
 from tkinter import Tk, Label
 from datetime import datetime
 import calendar
+import random
+colors=["#800000","#D2691E","#000080","#FFFACD","SteelBlue","DarkOliveGreen","#F08080","#FFFF00","#BDB76B","#FF4500","#C71585"]
+
+def random_color_of_window():
+    """Change window's color every time when it is opened"""
+    return random.choice(colors)
 
 def get_calendar():
+    """Get date and calendar on month"""
     now = datetime.now()
     year, month, day = now.year, now.month, now.day
     cal_text = calendar.month(year, month)
@@ -10,14 +17,18 @@ def get_calendar():
     return cal_text, date
 
 root = Tk()
+root.minsize(350,50)
 root.title("CALENDAR")
-root.configure(bg="#6495ED")
+
+root_color=random_color_of_window()
+root.configure(bg=root_color)
+
 calendar_text, today_data = get_calendar()
 
-cal_label = Label(root, text=calendar_text, font=("Courier", 12), justify="left")
+cal_label = Label(root, text=calendar_text, font=("Courier", 17), justify="left")
 cal_label.pack()
 
-date_label = Label(root, text=today_data, font=("Arial", 14), fg="white", bg="#6495ED")
+date_label = Label(root, text=today_data, font=("Arial", 17), fg="white", bg="#808080")
 date_label.pack()
 
 root.mainloop()
